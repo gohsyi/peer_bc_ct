b79b410efa4362183d448a786643d52866e45547
gohsyi <guohongyi@sjtu.edu.cn>
2020-05-25 02:50:52 +0800

    update bash

--- generate_expert_data.sh
+++ generate_expert_data.sh
@@ -2,5 +2,8 @@
 
 for ((i=1; i<10; i++));
 do
-  python -m stable_baselines.gail.dataset.record_expert logs/PongNoFrameskip-v4/baseline/rl_model_${i}000000_steps.zip --logdir logs/PongNoFrameskip-v4/baseline/${i}000000_steps/
+  python -m stable_baselines.gail.dataset.record_expert \
+  logs/$1NoFrameskip-v4/baseline/rl_model_${i}000000_steps.zip \
+  --note baseline/${i}e6_steps \
+  --env $1NoFrameskip-v4
 done;
