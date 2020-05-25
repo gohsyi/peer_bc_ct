f6a9b2638ac1c00c4a8b5e6ab991ecf5aa4e2bc9
gohsyi <guohongyi@sjtu.edu.cn>
2020-05-25 10:39:58 +0800

    update bash

--- peer_behavior_clone.sh
+++ peer_behavior_clone.sh
@@ -3,10 +3,8 @@
 for ((i=9; i>0; i--));
 do
   python -m stable_baselines.ppo2.peer_behavior_clone \
-  logs/PongNoFrameskip-v4/baseline/${i}000000_steps/expert.npz \
-  --logdir logs/PongNoFrameskip-v4/${i}000000_steps_peer1/ \
-  --num-epochs 50 \
-  --val-interval 100 \
-  --val-episodes 10 \
-  --peer 1
+  logs/$1NoFrameskip-v4/baseline/${i}e6_steps/expert.npz \
+  --env $1NoFrameskip-v4 \
+  --note ${i}e6_steps_peer$2/ \
+  --peer $2
 done;
